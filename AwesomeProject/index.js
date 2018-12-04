d4c5de03abc3ba616844126bed31aff27c1c4a26
naoki_katsumi <naoki_katsumi@2fc-group.jp>
2018-12-04 21:20:21 +0900

    Add NativeBase

--- AwesomeProject/index.js
+++ AwesomeProject/index.js
@@ -1,6 +1,9 @@
 import { AppRegistry } from 'react-native';
 import React from 'react';
 import { Provider } from 'react-redux';
+import { StyleProvider } from 'native-base'
+import getTheme from './native-base-theme/components'
+import platform from './native-base-theme/variables/platform'
 
 import Counter from './src/containers/Container';
 import store from './src/store';
@@ -10,5 +13,13 @@ const Application = () => (
     <Counter />
   </Provider>
 );
+
+// const Application = () => (
+//   <StyleProvider style={getTheme(material)}>
+//     <Provider store={store}>
+//       <Counter />
+//     </Provider>
+//   </StyleProvider>
+// );
 
 AppRegistry.registerComponent('AwesomeProject', () => Application);
--- AwesomeProject/src/modules/Counter/Counter.tsx
+++ AwesomeProject/src/modules/Counter/Counter.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { CounterState } from './Counter.module';
-import { Platform, StyleSheet, View, Text, Button } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
 import { ActionDispatcher } from '../../containers/Container';
 
 interface Props {
@@ -8,15 +9,65 @@ interface Props {
   actions: ActionDispatcher;
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    margin: 20
+  },
+  beer: {
+    marginLeft: -25,
+  },
+  button: {
+    marginTop: 10,
+  }
+});
+
+const buttnStyle = [{
+  marginTop: 10
+}];
+
 export class Counter extends React.Component<Props, {}> {
 
   render() {
     return (
-      <View>
-        <Text>score: {this.props.value.num}</Text>
-        <Button onPress={() => this.props.actions.increment(3)} title="Increment 3" />
-        <Button onPress={() => this.props.actions.decrement(2)} title="Decrement 2" />
-      </View>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <View style={styles.content}>
+            <Text>This is Content Section</Text>
+            <Text>score: {this.props.value.num}</Text>
+            <Button block success onPress={() => this.props.actions.increment(3)} style={buttnStyle}>
+              <Text>Increment 3</Text>
+            </Button>
+            <Button block warning onPress={() => this.props.actions.decrement(2)} style={buttnStyle}>
+              <Text>Decrement 2</Text>
+            </Button>
+          </View>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
     );
   }
 }
