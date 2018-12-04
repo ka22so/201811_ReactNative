2f41abf0fed34bfc71bded8f46fbecac5aa6a1e2
naoki_katsumi <naoki_katsumi@2fc-group.jp>
2018-12-04 18:58:09 +0900

    Add 'remote-redux-devtools';

--- AwesomeProject/src/store/index.ts
+++ AwesomeProject/src/store/index.ts
@@ -2,15 +2,16 @@ import { Reducers, ModuleAction, ModuleState } from '../modules';
 import { createStore, combineReducers, Action, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 const loggerMiddleware = createLogger();
 
 export default createStore(
   Reducers,
-  applyMiddleware(
+  composeWithDevTools(applyMiddleware(
     thunkMiddleware,
     loggerMiddleware
-  )
+  ))
 );
 
 export type ReduxState = ModuleState;
